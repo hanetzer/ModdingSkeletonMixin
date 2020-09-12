48ac874b254a364bf254c70a847e663f72f8bfd0
Marty E. Plummer <hanetzer@startmail.com>
2020-09-11 20:16:38 -0500

    mixin issue demo
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- src/main/java/com/example/examplemod/mixin/ExampleMixin.java
+++ src/main/java/com/example/examplemod/mixin/ExampleMixin.java
@@ -1,15 +1,42 @@
 package com.example.examplemod.mixin;
 
-import net.minecraft.client.gui.screen.MainMenuScreen;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.brain.task.Task;
+import net.minecraft.util.WeightedList;
+import net.minecraft.world.server.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(MainMenuScreen.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
+	@Mixin(targets = "net/minecraft/entity/ai/brain/task/MultiTask$RunType$RUN_ONE")
+	public static class RunOneMixin {
+		/**
+		 * @reason example
+		 * @author hanetzer
+		 */
+		@Overwrite(remap = false)
+		public void func_220630_a(WeightedList<Task<? super LivingEntity>> tasks, ServerWorld world, LivingEntity entity, long time) {
+			tasks.func_220655_b().filter((task) -> {
+				return task.getStatus() == Task.Status.STOPPED;
+			}).filter((task) -> {
+				return task.start(world, entity, time);
+			}).findFirst();
+		}
+	}
+
+	@Mixin(targets = "net/minecraft/entity/ai/brain/task/MultiTask$RunType$2;")
+	public static class TryAllMixin {
+		/**
+		 * @reason example
+		 * @author hanetzer
+		 */
+		@Overwrite(remap = false)
+		public <E extends LivingEntity> void func_220630_a(WeightedList<Task<? super E>> tasks, ServerWorld world, E entity, long time) {
+			tasks.func_220655_b().filter((task) -> {
+				return task.getStatus() == Task.Status.STOPPED;
+			}).forEach((task) -> {
+				task.start(world, entity, time);
+			});
+		}
 	}
 }
