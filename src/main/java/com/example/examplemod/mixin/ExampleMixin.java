6a567b8dbbfec7d7af4664171854876f0fa9aa75
Marty E. Plummer <hanetzer@startmail.com>
2020-09-12 11:32:53 -0500

    fix from @gabizou
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- src/main/java/com/example/examplemod/mixin/ExampleMixin.java
+++ src/main/java/com/example/examplemod/mixin/ExampleMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
 public class ExampleMixin {
-	@Mixin(targets = "net/minecraft/entity/ai/brain/task/MultiTask$RunType$RUN_ONE")
+	@Mixin(targets = "net/minecraft/entity/ai/brain/task/MultiTask$RunType$1")
 	public static class RunOneMixin {
 		/**
 		 * @reason example
@@ -24,13 +24,13 @@ public class ExampleMixin {
 		}
 	}
 
-	@Mixin(targets = "net/minecraft/entity/ai/brain/task/MultiTask$RunType$2;")
+	@Mixin(targets = "net/minecraft/entity/ai/brain/task/MultiTask$RunType$2")
 	public static class TryAllMixin {
 		/**
 		 * @reason example
 		 * @author hanetzer
 		 */
-		@Overwrite(remap = false)
+		@Overwrite
 		public <E extends LivingEntity> void func_220630_a(WeightedList<Task<? super E>> tasks, ServerWorld world, E entity, long time) {
 			tasks.func_220655_b().filter((task) -> {
 				return task.getStatus() == Task.Status.STOPPED;
